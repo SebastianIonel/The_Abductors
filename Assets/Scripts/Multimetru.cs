b6e7ada9aac9c4282ec640b113625dfb482bbc49
dariuusss <119854575+dariuusss@users.noreply.github.com>
2024-07-24 10:23:24 +0300

    inceput minigame

---
+++ Assets/Scripts/Multimetru.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using TMPro;
+
+public class Multimeter : MonoBehaviour
+{
+    public TMP_Text displayText; // Asociaza acest text in Inspector
+    private float measuredResistance;
+
+    // Metoda pentru simularea masurarii rezistentei
+    public void MeasureResistance(GameObject resistanceObject)
+    {
+        Resistance resistance = resistanceObject.GetComponent<Resistance>();
+        if (resistance != null)
+        {
+            measuredResistance = resistance.resistanceValue;
+            UpdateDisplay();
+        }
+    }
+
+    // Actualizeaza textul afisat cu valoarea masurata a rezistentei
+    void UpdateDisplay()
+    {
+        displayText.text = "Measured Resistance: " + measuredResistance + " Ohms";
+    }
+}
---
+++ Assets/Scripts/Rezistenta.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class Resistance : MonoBehaviour {
+    public float resistanceValue = 100f;
+}
+
